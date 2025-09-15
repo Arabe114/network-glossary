ccacedfbf78bab004055b678c840eb0045892801
Emir <emir@example.com>
2025-09-15 16:49:45 -0300

    Removed auth

--- auth.js
+++ auth.js
@@ -1,9 +1,6 @@
-// Authentication configuration
-// This file contains encoded credentials for security
 const AUTH_CONFIG = {
-    // Base64 encoded credentials - decode to verify
-    u: 'YWRtaW4=', // admin
-    p: 'bHV4dGVsQG5vY0AxOTQ1', // luxtel@noc@1945
+    u: 'YWRtaW4=',
+    p: 'bHV4dGVsQG5vY0AxOTQ1',
     
     // Alternative obfuscation method
     getCredentials: function() {
@@ -13,12 +10,10 @@ const AUTH_CONFIG = {
         };
     },
     
-    // Validation function
     validate: function(inputUser, inputPass) {
         const creds = this.getCredentials();
         return inputUser === creds.username && inputPass === creds.password;
     }
 };
 
-// Export for use in login
 window.AUTH_CONFIG = AUTH_CONFIG;
